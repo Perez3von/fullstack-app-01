a506bdef8bc0024f93ba96b4c57489e84074b259
Evon Perez <perez.evon@gmail.com>
2021-08-17 16:38:58 -0400

    adding some components

---
+++ src/CreatePerson.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class CreatePerson extends Component {
+    
+    render() { 
+        return ( 
+        <>
+        </> );
+    }
+}
+ 
+export default CreatePerson;
---
+++ src/PeopleDetail.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PeopleDetails extends Component {
+    
+    render() { 
+        return (
+
+            <>
+            </>
+          );
+    }
+}
+ 
+export default PeopleDetails;
